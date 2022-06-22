404eddb103189c681071a355731721e9715995bb
ogonz801 <g.osvaldo96@gmail.com>
2022-06-21 21:08:32 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -18,11 +18,9 @@ require('dotenv').config()
 require('./config/database')
 require('./config/googlePassport')
 
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs')
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
